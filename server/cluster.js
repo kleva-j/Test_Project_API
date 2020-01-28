f001c38294209d8fc8030b2326c986c70a95567d
kleva-j <kasmickleva@gmail.com>
2020-01-28 23:42:41 +0100

    Initial commit

---
+++ server/cluster.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable global-require */
+import cluster from 'cluster';
+import os from 'os';
+
+if (cluster.isMaster) {
+  const cpus = os.cpus().length;
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork();
+  }
+} else {
+  require('./index');
+}
